dfa99a2655d56f572ef55128457833c9f872d9b9
Laxman2916 <89543411+Laxman2916@users.noreply.github.com>
2021-11-03 22:17:41 -0500

    fwb00janumula

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var vehicleRouter = require('./routes/vehicle');
+var addmodsRouter = require('./routes/addmods');
+var selectorRouter = require('./routes/selector');
+
 
 var app = express();
 
@@ -21,6 +25,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/vehicle', vehicleRouter);
+app.use('/addmods', addmodsRouter);
+app.use('/selector', selectorRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
